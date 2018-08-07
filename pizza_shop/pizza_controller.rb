c0f14774ca85e7c8a17180a7ff04dc4c590b3ac6
Fiona Wilson <fionaw237@gmail.com>
2018-08-07 13:20:48 +0100

    Adds morning work

---
+++ pizza_shop/pizza_controller.rb
@@ -0,0 +1,23 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+
+require_relative('./models/pizza_order')
+also_reload('./models/*')
+
+# INDEX
+get '/pizza-orders' do
+  @orders = PizzaOrder.all()
+  erb(:index)
+end
+
+# NEW
+get '/pizza-orders/new' do
+  erb(:new)
+end
+
+# SHOW
+get '/pizza-orders/:id' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+end
